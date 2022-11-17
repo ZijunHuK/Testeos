372f87a12c79e353bef16ba97f5a526bbf1f429e
Zijun Hu <zijun.hu@tiernoparla.es>
2022-11-17 21:19:18 +0100

    guardar

---
+++ src/main/java/com/tiernoparla/testeos/Calculadora.java
@@ -0,0 +1,10 @@
+
+package com.tiernoparla.testeos;
+
+public class Calculadora {
+    
+    public int sumar(int num1, int num2) {
+        return num1 * num2;
+    } //sumar
+    
+}// Calculadora
